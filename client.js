3b9c6a54710cace290e332d1f12b2b9b78b79385
Yonis7 <yonisaden7@gmail.com>
2023-01-21 01:21:03 +0000

    Stdin Setup

--- client.js
+++ client.js
@@ -10,18 +10,16 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
-  conn.on("connect", (data) => {
-    console.log(data)
-  });
+  
 
   conn.on("connect", () => {
     console.log('Connection successful')
     conn.write(`Name: YA7`)
     // conn.write(`Move: up`)
   })
-
-  conn.on('data', () => {
-    console.log(data);
+  
+  conn.on("data", (data) => {
+    console.log(data)
   });
 
   return conn;
--- play.js
+++ play.js
@@ -1,6 +1,30 @@
 const net = require("net");
-
 const connect = require('./client')
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.on("data", handleUserInput);
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+
+  
+
+};
+
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    console.log('process exitor')
+    process.exit();
+  }
+}
+
 
 console.log("Connecting ...");
 connect();
+
+
+setupInput()
